3ada1f689bfee8a18e8d9ea5a16e4893a2cf7a4e
griendt <avdg.lucerna@gmail.com>
2020-07-02 22:34:46 +0200

    Move CardCollection to own file and implement len and iadd

--- structs/card.py
+++ structs/card.py
@@ -8,14 +8,3 @@ class Card(NamedTuple):
     suit: Suit
     rank: Rank
 
-
-class CardCollection(object):
-    cards: List[Card]
-
-    def __init__(self, cards: List[Card]):
-        self.cards = cards
-
-    def __eq__(self, other):
-        # The order in which the cards are listed is not relevant for equality.
-        # In card collections in which the order is relevant, you should override this method.
-        return set(self.cards) == set(other.cards)
