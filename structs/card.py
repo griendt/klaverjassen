357a6af73c63ca7571399b8e9ee17287c55d9f85
griendt <avdg.lucerna@gmail.com>
2020-07-02 21:58:51 +0200

    Add docstrings

--- structs/card.py
+++ structs/card.py
@@ -2,6 +2,8 @@ from typing import NamedTuple, List
 from enum import Enum
 import random
 
+from structs.player import Player
+
 
 class Suit(Enum):
     CLUBS = 1
@@ -33,32 +35,56 @@ class CardCollection(object):
         self.cards = cards
 
     def __eq__(self, other):
+        # The order in which the cards are listed is not relevant for equality.
+        # In card collections in which the order is relevant, you should override this method.
         return set(self.cards) == set(other.cards)
+
 
 class Deck(CardCollection):
     seed: int = None
 
-    def shuffle(self, seed: int = None):
+    def __init__(self, cards: CardCollection = None):
+        """
+        Initializes a deck. The cards in it are in the order that it is given.
+        To randomize the order of the deck, call `shuffle`.
+
+        :param cards: The cards that the deck holds. If this parameter is not specified,
+        a full piquet deck (7-A of all four suits) is used by default.
+        """
+
+        # Default the card collection to a full deck if none are specified.
+        cards = [
+            Card(suit=suit, rank=rank)
+            for suit in Suit
+            for rank in Rank
+        ] if cards is None else cards
+        super().__init__(cards)
+
+    def shuffle(self, seed: int = None) -> None:
+        """
+        Shuffle the deck's cards according to the seed.
+        If no seed is given, a random seed is used.
+
+        :param seed: The seed to use for the RNG.
+        """
         rng = random.Random()
         self.seed = seed if seed is not None else rng.randint(0, 100000000)
         rng.seed(self.seed)
         rng.shuffle(self.cards)
 
+    def deal(self, players: List[Player]) -> None:
+        """
+        Deal the deck among the players.
+
+        :param players: The list of players. The cards are dealt in the order in which the players are given.
+        """
+        if len(self.cards) % len(players) != 0:
+            raise ValueError(f"Cannot evenly deal {len(self.cards)} cards among {len(players)} players")
+
     def __repr__(self):
         return repr(self.cards)
-
-    def __init__(self, cards: CardCollection = None):
-        if cards is None:
-            self.cards = [
-                Card(suit=suit, rank=rank)
-                for suit in Suit
-                for rank in Rank
-            ]
-        else:
-            self.cards = cards
 
     def __eq__(self, other):
         # In case different seeds result in the same card order,
         # we should of course consider the decks equal anyway.
         return self.cards == other.cards
-
--- structs/player.py
+++ structs/player.py
@@ -8,12 +8,23 @@ class Player(object):
     name: str
 
     def __init__(self, hand: CardCollection = None, name: str = None):
+        """
+        Initialize a player.
+
+        :param hand: The hand of the player. If left unspecified, an empty hand is assumed.
+        :param name: The given name of the player. If left unspecified, a random name is generated.
+        """
+
         self.hand = hand if hand is not None else CardCollection([])
         self.name = name if name is not None else self.generate_name()
 
     @staticmethod
     def generate_name() -> str:
-        """Generates a random, pronouncable name for a player."""
+        """Generates a random, pronouncable name for a player.
+
+        :return: A random name.
+        """
+
         vowels = ["a", "e", "o"] * 2 + ["i", "u"]
         consonants = ["b", "d", "f", "g", "h", "k", "l", "m", "n", "p", "qu", "r", "s", "t", "v", "y"]
 
