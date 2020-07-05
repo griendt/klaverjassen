b249f59993af01208adf02a82b967bff4f525524
griendt <avdg.lucerna@gmail.com>
2020-07-05 14:55:26 +0200

    Add tests for asking a trump suit upon round start

--- test/test_round.py
+++ test/test_round.py
@@ -1,8 +1,9 @@
 import unittest
 
+from interaction import mock_input
 from models.player import Player
 from models.round import Round
-from models.suit import Suit
+from models.suit import Suit, suits
 
 
 class RoundTestCase(unittest.TestCase):
@@ -32,6 +33,27 @@ class RoundTestCase(unittest.TestCase):
 
         for suit in Suit:
             Round(players=players, bidder_index=0, trump_suit=suit)
+
+    def test_a_round_asks_for_trump_suit_when_started(self):
+        players = [Player(), Player(), Player(), Player()]
+
+        suit_identifier: str
+        suit: Suit
+        for (suit_identifier, suit) in suits().items():
+            with mock_input(suit_identifier):
+                round = Round(players=players, bidder_index=0)
+                round.initialize()
+
+                self.assertEqual(round.trump_suit, suit)
+
+    def test_a_round_fails_to_start_if_invalid_trump_suit_is_given(self):
+        def bad_initialization():
+            players = [Player(), Player(), Player(), Player()]
+            with mock_input("bad_suit"):
+                round = Round(players=players, bidder_index=0)
+                round.initialize()
+
+        self.assertRaises(KeyError, bad_initialization)
 
 
 if __name__ == "__main__":
