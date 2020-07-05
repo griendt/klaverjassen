edc440f0dc14c80b9cf19045672e8940c65ac1d1
griendt <avdg.lucerna@gmail.com>
2020-07-05 19:35:03 +0200

    Unit test for ability to play a card

--- test/test_trick.py
+++ test/test_trick.py
@@ -1,6 +1,6 @@
 import unittest
 
-from models import Player, Trick
+from models import Player, Trick, Card, Rank, Suit
 
 
 class RoundTestCase(unittest.TestCase):
@@ -28,6 +28,20 @@ class RoundTestCase(unittest.TestCase):
         trick = Trick(players=players, leading_player_index=0)
 
         self.assertEqual(None, trick.led_suit)
+
+    def test_a_card_can_be_played_in_a_trick(self):
+        players = [Player(), Player(), Player(), Player()]
+        trick = Trick(players=players, leading_player_index=0)
+
+        players[0].hand = {Card(suit=Suit.SPADES, rank=Rank.ACE)}
+        trick.play(Card(suit=Suit.SPADES, rank=Rank.ACE))
+
+        # The turn must shift to the next player
+        self.assertEqual(1, trick.player_index_to_play)
+        # The card must be removed from the player's hand
+        self.assertEqual(0, len(players[0].hand))
+        # The card resides within the played cards of the trick
+        self.assertEqual(True, Card(suit=Suit.SPADES, rank=Rank.ACE) in trick.played_cards)
 
 
 if __name__ == "__main__":
