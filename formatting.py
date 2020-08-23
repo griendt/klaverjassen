4ed9260e82c6336529361654595770002ed4b9c2
Griendt <alex@e-cube.nl>
2020-08-23 15:31:55 +0200

    Add MyPy warnings to formatting helper script

--- formatting.py
+++ formatting.py
@@ -2,3 +2,4 @@ import subprocess
 
 if __name__ == "__main__":
     subprocess.run(["python", "-m", "black", ".", "-l120", "-tpy38"])
+    subprocess.run(["python", "-m", "mypy", "--disallow-untyped-defs", "--disallow-any-generics", "."])
